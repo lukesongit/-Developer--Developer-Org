5e806878aa215f658e4e5dc8a55ac0850fb449ce
Luke H <its_just_luke@outlook.com>
2022-05-29 13:09:53 -0400

    green tea test

---
+++ Developer Project/force-app/main/default/lwc/greenTea6/greenTea6.js
@@ -0,0 +1,4 @@
+import { LightningElement } from 'lwc';
+export default class Chai6 extends LightningElement {
+    pictureUrl = 'https://sdodemo-main-166ce2cf6b6-172-1799ff97a56.force.com/b2b2c/s/sfsites/c/cms/delivery/media/MCUXP4SHLWRZCMZGBTT23A3XO5ZA?recordId=01t3y000007TGVr&buyerId=000000000000000';
+}
