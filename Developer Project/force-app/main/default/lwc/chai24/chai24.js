046b5b9a71a785dcb65a5c7ff9d4ae36d515f508
Luke H <its_just_luke@outlook.com>
2022-05-29 12:40:59 -0400

    chai has been added ready to test

---
+++ Developer Project/force-app/main/default/lwc/chai24/chai24.js
@@ -0,0 +1,4 @@
+import { LightningElement } from 'lwc';
+export default class Chai24 extends LightningElement {
+    pictureUrl = 'https://sdodemo-main-166ce2cf6b6-172-1799ff97a56.force.com/b2b2c/s/sfsites/c/cms/delivery/media/MCVVZDVTMT3VBNHIT7MCFK46DUEM?recordId=01t3y000007TGWN&buyerId=000000000000000';
+}
---
+++ Developer Project/force-app/main/default/lwc/chai6/chai6.js
@@ -0,0 +1,4 @@
+import { LightningElement } from 'lwc';
+export default class Chai6 extends LightningElement {
+    pictureUrl = 'https://s3.amazonaws.com/sfdc-ckz-b2b/RCG/CG/Alpine+Products/EnergyDrinks/Pre/6Pack-Chai-Pre-Large.jpg';
+}
