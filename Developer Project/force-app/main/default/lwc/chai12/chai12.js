31d74f15cbecb060094db597e9b54bd519643a87
Luke H <its_just_luke@outlook.com>
2022-05-29 12:59:31 -0400

    chai is done and ready to be implemented

--- Developer Project/force-app/main/default/lwc/chai12/chai12.js
+++ Developer Project/force-app/main/default/lwc/chai12/chai12.js
@@ -1,4 +1,4 @@
 import { LightningElement } from 'lwc';
 export default class Chai12 extends LightningElement {
-    pictureUrl = 'https://s3.amazonaws.com/sfdc-ckz-b2b/RCG/CG/Alpine+Products/EnergyDrinks/Pre/6Pack-Chai-Pre-Large.jpg';
+    pictureUrl = 'https://s3.amazonaws.com/sfdc-ckz-b2b/RCG/CG/Alpine+Products/EnergyDrinks/Post/12Pack-Chai-Post-Large.png';
 }
--- Developer Project/force-app/main/default/lwc/chai24/chai24.js
+++ Developer Project/force-app/main/default/lwc/chai24/chai24.js
@@ -1,4 +1,4 @@
 import { LightningElement } from 'lwc';
 export default class Chai24 extends LightningElement {
-    pictureUrl = 'https://sdodemo-main-166ce2cf6b6-172-1799ff97a56.force.com/b2b2c/s/sfsites/c/cms/delivery/media/MCVVZDVTMT3VBNHIT7MCFK46DUEM?recordId=01t3y000007TGWN&buyerId=000000000000000';
+    pictureUrl = 'https://sdodemo-main-166ce2cf6b6-172-1799ff97a56.force.com/b2b2c/s/sfsites/c/cms/delivery/media/MCUTY7HACLDJE5DCAYYJM4R7EW7M?recordId=01t3y000007TGWL&buyerId=000000000000000';
 }
