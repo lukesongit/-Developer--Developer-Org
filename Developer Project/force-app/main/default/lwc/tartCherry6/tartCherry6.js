ce2a64d5e8d8d436350f1181049f69596802e2ac
Luke H <its_just_luke@outlook.com>
2022-05-29 10:44:31 -0400

    js file cleanup

--- Developer Project/force-app/main/default/lwc/tartCherry6/tartCherry6.js
+++ Developer Project/force-app/main/default/lwc/tartCherry6/tartCherry6.js
@@ -1,3 +1,4 @@
 import { LightningElement } from 'lwc';
+export default class TartCherry6 extends LightningElement {
     picureUrl = 'https://s3.amazonaws.com/sfdc-ckz-b2b/RCG/CG/Alpine+Products/EnergyDrinks/Pre/6Pack-Tart-Cherry-Pre-Large.jpg';
-export default class TartCherry6 extends LightningElement {}
+}
