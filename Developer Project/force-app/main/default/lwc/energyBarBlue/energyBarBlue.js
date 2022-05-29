3457dd439c475d1f695eca2577566a1089d5c0a7
Luke H <its_just_luke@outlook.com>
2022-05-29 17:44:25 -0400

    Deliverable A complete

---
+++ Developer Project/force-app/main/default/lwc/energyBarBlue/energyBarBlue.js
@@ -0,0 +1,4 @@
+import { LightningElement } from 'lwc';
+export default class EnergyBarBlue extends LightningElement {
+    pictureUrl = 'https://s3.amazonaws.com/sfdc-ckz-b2b/RCG/CG/Alpine+Products/EnergyBars/BigFootBars/BarsBoxof24_Bigfoot_Blueberry-Pistachio_Large.png';
+}
