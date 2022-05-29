4a3514361ec574122349040aec8b0ea5f3f4ecaa
Luke H <its_just_luke@outlook.com>
2022-05-29 10:17:08 -0400

    tart cherry six pack

---
+++ Developer Project/force-app/main/default/lwc/tartCherry6/tartCherry6.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+    picureUrl = 'https://s3.amazonaws.com/sfdc-ckz-b2b/RCG/CG/Alpine+Products/EnergyDrinks/Pre/6Pack-Tart-Cherry-Pre-Large.jpg';
+export default class TartCherry6 extends LightningElement {}
